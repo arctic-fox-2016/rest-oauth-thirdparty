94729fca9fa9e5839fbd2a633bacad67e2a9e60a
tevinstein <tevinsa@yahoo.com>
2016-09-30 06:04:33 +0700

    sudah selesai

---
+++ config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'      : '303519716687078',
+        'clientSecret'  : '1ad049b55b31f27c3fc552ba4690b901',
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'       : '53RDB2QQNgV9035pPr3Tw10I9',
+        'consumerSecret'    : 'r9bgBIOvKuXOkE1rsNYeawqQshNlMzsYufAiAAWPMPJk3AclcE',
+        'callbackURL'       : 'http://127.0.0.1:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'      : '699627141311-ki03v4ic5fcepuvalqr0cvncvqocpgi6.apps.googleusercontent.com',
+        'clientSecret'  : 'LV17pk8qsoKaWo3zuHpfGJt3',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
